9116c9e014046e4348362ccd448877842cb6df04
ilame <ilhammaj@gmail.com>
2015-02-07 16:02:18 +0000

    add plot1.R

---
+++ plot1.R
@@ -0,0 +1,6 @@
+require(sqldf)
+file<-c("./household_power_consumption.txt")
+data_subset <- read.csv.sql(file, header = T, sep=";", sql = "select * from file where (Date == '1/2/2007' OR Date == '2/2/2007')" )
+png("plot1.png",width=480,height=480,units="px")
+hist(data_subset$Global_active_power, breaks=12,col="red", main="Global Active Power",xlab="Global Active Power(kilowatts)")
+dev.off()
