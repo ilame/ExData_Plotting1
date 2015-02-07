f8b9490c1880cf89ad756b257567536c9cb3f34c
ilame <ilame@users.noreply.github.com>
2015-02-07 20:16:13 +0000

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,5 +1,6 @@
 require(sqldf)
 file <- c("./household_power_consumption.txt")
+##read.csv.sql function Reads a file into R filtering it with an sql statement. Only the filtered portion is processed by R
 data_subset <- read.csv.sql(file, header = T, sep=";", sql = "select * from file where (Date == '1/2/2007' OR Date == '2/2/2007')" )
 datetime <- strptime(paste(data_subset$Date, data_subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 png("plot2.png",width=480,height=480,units="px")
