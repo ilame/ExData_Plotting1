65ae41c4693c1f2ece860c5be9a05104ca58db1f
ilame <ilhammaj@gmail.com>
2015-02-07 16:11:10 +0000

    add plot2.R

---
+++ plot2.R
@@ -0,0 +1,7 @@
+require(sqldf)
+file <- c("./household_power_consumption.txt")
+data_subset <- read.csv.sql(file, header = T, sep=";", sql = "select * from file where (Date == '1/2/2007' OR Date == '2/2/2007')" )
+datetime <- strptime(paste(data_subset$Date, data_subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+png("plot2.png",width=480,height=480,units="px")
+plot(dateTime, data_subset$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
